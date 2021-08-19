88163feef3df3d79159b76d874be3064677c44ac
Matic Matušek <maticmatusek@gmail.com>
2021-08-19 14:04:53 +0200

    Skoraj dokončana model in spletni vmesnik

--- model.py
+++ model.py
@@ -8,40 +8,40 @@ import json
 #NI V REDU POGLEJ POD  METODO UDAREC
 DATOTEKA_S_STANJEM = "stanje.json"
 
+
 class Igra:
 
-    def __init__(self, pozicija_luknje, igralec_na_vrsti, koordinate_zacetka, igralci = None ,koncani_igralci = None, runda = None, ):
+    def __init__(self, pozicija_luknje, igralec_na_vrsti, koordinate_zacetka, igralci=None, koncani_igralci=None, runda=None, ):
         if igralci is None:
             self.igralci = {}
-        else :
+        else:
             self.igralci = igralci
         if runda is None:
             self.runda = 0
-        else :
+        else:
             self.runda = runda
         if koncani_igralci is None:
-            self.koncani_igralci = {}
-        else :
+            self.koncani_igralci = []
+        else:
             self.koncani_igralci = koncani_igralci
         self.pozicija_luknje = pozicija_luknje
         self.igralec_na_vrsti = igralec_na_vrsti
         self.koordinate_zacetka = koordinate_zacetka
 
-
-
     #def __str__(self):
-     #  
+     #
       #  return f"Pozicija luknje je:{self.pozicija_luknje} \n Igralec, ki je na vrsti je igralec: {self.igralci[self.igralec_na_vrsti][0]}\n Številka: {self.igralec_na_vrsti}\n Runda : {self.runda} \n Začetne koordinate so {self.koordinate_zacetka}\n Igralci so {self.igralci}"
 
     def dodaj_igralca(self, ime_igralca):
         stevilo_udarcev = 0
         stevilka_igralca = len(self.igralci)
         koordinata_x, koordinata_y = self.koordinate_zacetka
-        self.igralci[stevilka_igralca] = [ime_igralca, (koordinata_x, koordinata_y), stevilo_udarcev]
+        self.igralci[stevilka_igralca] = [ime_igralca,
+                                          (koordinata_x, koordinata_y), stevilo_udarcev]
         #doda igralca med množico igralcev in jih oštevilči od skupaj z 0 naprej
 
     def udarec(self, moc, kot):
-        if moc > 100 or moc < 0 :
+        if moc > 100 or moc < 0:
             return "Moč je vrednost med 0 in 100"
         self.nova_runda()
         premik_x, premik_y = izracun_premika(moc, kot)
@@ -52,15 +52,15 @@ class Igra:
         nove_koordinate = self.preveri_isto_mesto(novi_x, novi_y)
         posodobljen_x, posodobljen_y = nove_koordinate
         if preveri_udarec(posodobljen_x, posodobljen_y):
-            self.igralci[self.igralec_na_vrsti][1] = (posodobljen_x, posodobljen_y)
+            self.igralci[self.igralec_na_vrsti][1] = (
+                posodobljen_x, posodobljen_y)
 
             self.naslednji_igralec()
-            self.igralec_koncal() 
+            self.igralec_koncal()
         else:
             self.naslednji_igralec()
-            self.igralec_koncal() 
+            self.igralec_koncal()
             return "Žogica je odletela izven meja"
-
 
     def naslednji_igralec(self):
         if self.igralec_na_vrsti < max(self.igralci.keys()):
@@ -76,12 +76,13 @@ class Igra:
             mnozica.add(self.igralci[i][2])
         self.runda = min(mnozica) + 1
         #ko imajo vsi igralci isto število udarcev, se runda zamenja
-        
+
     def igralec_koncal(self):
         for i in self.igralci.keys():
-            if self.igralci[i][1] == self.pozicija_luknje :
+            if self.igralci[i][1] == self.pozicija_luknje:
                 ime_igralca = self.igralci[i][0]
-                self.koncani_igralci[ime_igralca] = self.runda
+                koncani_igralec = ime_igralca, self.runda
+                self.koncani_igralci.append(koncani_igralec)
                 self.igralci.pop(i)
                 break
             else:
@@ -89,13 +90,13 @@ class Igra:
         #preveri, če je igralec končal in ga vrže ven iz igre,
         #ter ga doda na seznam igralcev, ki so končali skupaj s številom rund
 
-    def preveri_isto_mesto(self,novi_x, novi_y):
+    def preveri_isto_mesto(self, novi_x, novi_y):
         mnozica = set()
         nove_koordinate = novi_x, novi_y
         nove_koordinate1 = novi_x, novi_y + 1
         nove_koordinate2 = novi_x, novi_y - 1
         nove_koordinate3 = novi_x + 1, novi_y
-        nove_koordinate4 = novi_x -1, novi_y
+        nove_koordinate4 = novi_x - 1, novi_y
         nove_koordinate5 = novi_x + 1, novi_y + 1
         nove_koordinate6 = novi_x - 1, novi_y + 1
         nove_koordinate7 = novi_x + 1, novi_y - 1
@@ -120,19 +121,16 @@ class Igra:
             return nove_koordinate7
         elif nove_koordinate8 not in mnozica:
             return nove_koordinate8
-        
 
 
-def preveri_udarec(novi_x,novi_y):
-    if novi_x > 100 or novi_y > 100 or novi_x < 0 or novi_y < 0 :
+def preveri_udarec(novi_x, novi_y):
+    if novi_x > 100 or novi_y > 100 or novi_x < 0 or novi_y < 0:
         return False
     else:
         return True
 
 
-
-
-def nova_igra1(igra = random.randint(1,5)):
+def nova_igra1(igra=random.randint(1, 5)):
     if igra == 1:
         x_koordinata_luknje = random.randint(60, 100)
         y_koordinata_luknje = random.randint(1, 40)
@@ -151,29 +149,29 @@ def nova_igra1(igra = random.randint(1,5)):
         x_koordinata_zacetka = random.randint(60, 100)
         y_koordinata_zacetka = random.randint(60, 100)
         return Igra((x_koordinata_luknje, y_koordinata_luknje), 0, (x_koordinata_zacetka, y_koordinata_zacetka))
-    if igra == 4 :
+    if igra == 4:
         x_koordinata_luknje = random.randint(60, 100)
         y_koordinata_luknje = random.randint(60, 100)
         x_koordinata_zacetka = random.randint(1, 40)
         y_koordinata_zacetka = random.randint(1, 40)
         return Igra((x_koordinata_luknje, y_koordinata_luknje), 0, (x_koordinata_zacetka, y_koordinata_zacetka))
-    if igra == 5 :
-        pod_igra = random.randint(1,4)
+    if igra == 5:
+        pod_igra = random.randint(1, 4)
         x_koordinata_luknje = random.randint(35, 65)
         y_koordinata_luknje = random.randint(35, 65)
-        if pod_igra == 1 :
+        if pod_igra == 1:
             x_koordinata_zacetka = random.randint(1, 100)
             y_koordinata_zacetka = random.randint(1, 20)
             return Igra((x_koordinata_luknje, y_koordinata_luknje), 0, (x_koordinata_zacetka, y_koordinata_zacetka))
-        if pod_igra == 2 :
+        if pod_igra == 2:
             x_koordinata_zacetka = random.randint(1, 100)
             y_koordinata_zacetka = random.randint(80, 100)
             return Igra((x_koordinata_luknje, y_koordinata_luknje), 0, (x_koordinata_zacetka, y_koordinata_zacetka))
-        if pod_igra == 3 :
+        if pod_igra == 3:
             x_koordinata_zacetka = random.randint(1, 20)
             y_koordinata_zacetka = random.randint(1, 100)
             return Igra((x_koordinata_luknje, y_koordinata_luknje), 0, (x_koordinata_zacetka, y_koordinata_zacetka))
-        if pod_igra == 4 :
+        if pod_igra == 4:
             x_koordinata_zacetka = random.randint(80, 100)
             y_koordinata_zacetka = random.randint(1, 100)
             return Igra((x_koordinata_luknje, y_koordinata_luknje), 0, (x_koordinata_zacetka, y_koordinata_zacetka))
@@ -188,6 +186,7 @@ def izracun_premika(moc, kot):
     #izračuna premik glede na kot in moč,
     #pozor y os je pozitivna navzdol, zato se v metodi, udarec y odšteje
 
+
 class Golf:
 
     def __init__(self, datoteka_s_stanjem):
@@ -195,27 +194,26 @@ class Golf:
         self.datoteka_s_stanjem = datoteka_s_stanjem
 
     def prost_id_igre(self):
-        if len(self.igre) == 0 :
+        if len(self.igre) == 0:
             return 0
-        else :
-            return max(self.igre.keys()) + 1
+        else:
+            return len(self.igre.keys()) + 1
 
-    def udarec(self,id_igre, moc, kot):
+    def udarec(self, id_igre, moc, kot):
         self.nalozi_igre_iz_datoteke()
         igra = self.igre[id_igre]
-        igra.udarec(moc,kot)
+        igra.udarec(moc, kot)
         self.igre[id_igre] = igra
         self.zapisi_igre_v_datoteko()
 
-    def dodaj_igralca(self,id_igre,ime):
+    def dodaj_igralca(self, id_igre, ime):
         self.nalozi_igre_iz_datoteke()
         igra = self.igre[id_igre]
         igra.dodaj_igralca(ime)
         self.igre[id_igre] = igra
         self.zapisi_igre_v_datoteko()
 
-
-    def nova_igra(self, stevilka_igre = random.randint(1,5) ):
+    def nova_igra(self, stevilka_igre=random.randint(1, 5)):
         self.nalozi_igre_iz_datoteke()
         id_igre = self.prost_id_igre()
         igra = nova_igra1(stevilka_igre)
@@ -225,14 +223,21 @@ class Golf:
 
     def zapisi_igre_v_datoteko(self):
         with open(self.datoteka_s_stanjem, "w", encoding="utf-8") as f:
-            igre = {id_igre: (igra.igralci, igra.runda , igra.pozicija_luknje, igra.igralec_na_vrsti , igra.koordinate_zacetka, igra.koncani_igralci ) for id_igre, igra in self.igre.items()}
+            igre = {id_igre: (igra.igralci, igra.runda, igra.pozicija_luknje, igra.igralec_na_vrsti,
+                              igra.koordinate_zacetka, igra.koncani_igralci) for id_igre, igra in self.igre.items()}
             json.dump(igre, f)
-        
+
+    #def nalozi_igre_iz_datoteke(self):
+     #   with open(self.datoteka_s_stanjem, "r", encoding="utf-8") as f:
+      #      igre = json.load(f)
+       #     self.igre = {int(id_igre): (Igra(pozicija_luknje, igralec_na_vrsti, koordinate_zacetka, igralci, koncani_igralci, runda)
+        #                                for id_igre, (igralci, runda, pozicija_luknje, igralec_na_vrsti, koordinate_zacetka, koncani_igralci) in igre.items())}
+
     def nalozi_igre_iz_datoteke(self):
         with open(self.datoteka_s_stanjem, "r", encoding="utf-8") as f:
             igre = json.load(f)
-            self.igre = {int( id_igre ): (Igra(pozicija_luknje, igralec_na_vrsti, koordinate_zacetka, igralci, koncani_igralci, runda) for id_igre , (igralci,runda,pozicija_luknje, igralec_na_vrsti, koordinate_zacetka,koncani_igralci) in igre.items())}
-
+            for id_igre, (igralci, runda, pozicija_luknje, igralec_na_vrsti, koordinate_zacetka, koncani_igralci) in igre.items():
+                self.igre[id_igre] = Igra(pozicija_luknje, igralec_na_vrsti, koordinate_zacetka, igralci, koncani_igralci, runda)
 #I = nova_igra()
 #I.dodaj_igralca("Matic")
 #I.dodaj_igralca("Iza")
--- spletni-vmesnik.py
+++ spletni-vmesnik.py
@@ -4,80 +4,128 @@ SKRIVNOST = "skrivnost je skrivnost"
 
 golf = model.Golf(model.DATOTEKA_S_STANJEM)
 
+
 @bottle.get("/")
 def osnovna_stran():
     return bottle.template("osnovna.html")
 
-#@bottle.get("/igra")
-#def igra_stran():
-#    slovar = {}
-#    slovar_igralcev = nova_igra.igralci
-#    for i in slovar_igralcev:
-#        x_koordinata_luknje, y_koordinata_luknje = nova_igra.pozicija_luknje
-#        koordinate = slovar_igralcev[i][1]
-#        x_koordinata, y_koordinata = koordinate
-#        slovar[i] = koordinate
-#    return bottle.template("igra.html", slovar = slovar_igralcev ,x_koordinata_luknje =x_koordinata_luknje, y_koordinata_luknje = y_koordinata_luknje)
+@bottle.get("/igra")
+def igra_stran():
+    golf.nalozi_igre_iz_datoteke()
+    id_igre = int(bottle.request.get_cookie("idigre" , secret=SKRIVNOST).split("e")[1])
+    igra = golf.igre[id_igre]
+    igralci = igra.igralci
+    x_koordinata_luknje, y_koordinata_luknje = igra.pozicija_luknje
+    pozicija_luknje = igra.pozicija_luknje
+    igralec_na_vrsti = igra.igralec_na_vrsti
+    koordinate_zacetka = igra.koordinate_zacetka
+    runda = igra.runda
+    koncani_igralci = igra.koncani_igralci
+    return bottle.template("igra.html",igralci=igralci ,x_koordinata_luknje=x_koordinata_luknje,y_koordinata_luknje=y_koordinata_luknje,pozicija_luknje=pozicija_luknje,igralec_na_vrsti=igralec_na_vrsti,koordinate_zacetka=koordinate_zacetka,runda=runda,koncani_igralci=koncani_igralci)
+
 
 @bottle.post("/nova-igra/")
 def preusmeritev_na_izbiro_igre():
     bottle.redirect("/izbira-igre/")
 
+
 @bottle.get("/izbira-igre/")
 def izibra_igre():
     return bottle.template("izbira.html")
 
+
 @bottle.post("/izbrana-igra/1")
 def ena():
     id_igre = golf.nova_igra(1)
-    bottle.response.set_cookie("idigre" , "idigre{}".format(id_igre), path="/" , secret = SKRIVNOST )
+    bottle.response.set_cookie("idigre", "idigre{}".format(
+        id_igre), path="/", secret=SKRIVNOST)
     bottle.redirect("/dodajanje-igralcev/")
+
 
 @bottle.post("/izbrana-igra/2")
 def dva():
     id_igre = golf.nova_igra(2)
-    bottle.response.set_cookie("idigre" , "idigre{}".format(id_igre), path="/" , secret = SKRIVNOST )
+    bottle.response.set_cookie("idigre", "idigre{}".format(
+        id_igre), path="/", secret=SKRIVNOST)
     bottle.redirect("/dodajanje-igralcev/")
+
 
 @bottle.post("/izbrana-igra/3")
 def tri():
     id_igre = golf.nova_igra(3)
-    bottle.response.set_cookie("idigre" , "idigre{}".format(id_igre), path="/" , secret = SKRIVNOST )
+    bottle.response.set_cookie("idigre", "idigre{}".format(
+        id_igre), path="/", secret=SKRIVNOST)
     bottle.redirect("/dodajanje-igralcev/")
+
 
 @bottle.post("/izbrana-igra/4")
 def stiri():
     id_igre = golf.nova_igra(4)
-    bottle.response.set_cookie("idigre" , "idigre{}".format(id_igre), path="/" , secret = SKRIVNOST )
+    bottle.response.set_cookie("idigre", "idigre{}".format(
+        id_igre), path="/", secret=SKRIVNOST)
     bottle.redirect("/dodajanje-igralcev/")
+
 
 @bottle.post("/izbrana-igra/5")
 def pet():
     id_igre = golf.nova_igra(5)
-    bottle.response.set_cookie("idigre" , "idigre{}".format(id_igre), path="/" , secret = SKRIVNOST )
+    bottle.response.set_cookie("idigre", "idigre{}".format(
+        id_igre), path="/", secret=SKRIVNOST)
     bottle.redirect("/dodajanje-igralcev/")
+
+
+@bottle.post("/izbrana-igra/6")
+def random():
+    id_igre = golf.nova_igra()
+    bottle.response.set_cookie("idigre", "idigre{}".format(
+        id_igre), path="/", secret=SKRIVNOST)
+    bottle.redirect("/dodajanje-igralcev/")
+
 
 @bottle.get("/dodajanje-igralcev/")
 def stran_dodajanja():
-    return bottle.template("dodajanje-igralcev.html")
+    id_igre = int(bottle.request.get_cookie("idigre" , secret=SKRIVNOST).split("e")[1])
+    igra = golf.igre[id_igre]
+    igralci = igra.igralci
+    return bottle.template("dodajanje-igralcev.html", igra = igra, igralci = igralci)
+
 
 @bottle.post("/dodaj-igralca/")
 def dodaj_igralca():
     ime = bottle.request.forms["ime"]
+    id_igre = int(bottle.request.get_cookie("idigre" , secret=SKRIVNOST).split("e")[1])
     if ime:
-        golf.dodaj_igralca(ime)
+        golf.dodaj_igralca(id_igre, ime)
     else:
         return "ime mora biti ne prazno"
+    bottle.redirect("/dodajanje-igralcev/")
+
+
 @bottle.post("/udarec/")
 def udarec():
+    id_igre = int(bottle.request.get_cookie("idigre" , secret=SKRIVNOST).split("e")[1])
     moc = int(bottle.request.forms["moc"])
     kot = int(bottle.request.forms["kot"])
-    golf.udarec(moc,kot)
+    golf.udarec(id_igre, moc, kot)
     bottle.redirect("/igra")
+
 
 @bottle.get("/img/<picture>")
 def serve_picture(picture):
     return bottle.static_file(picture, root="img")
 
+@bottle.post("/rezultati/")
+def rezultati():
+    golf.nalozi_igre_iz_datoteke()
+    id_igre = int(bottle.request.get_cookie("idigre" , secret=SKRIVNOST).split("e")[1])
+    igra = golf.igre[id_igre]
+    igralci = igra.igralci
+    x_koordinata_luknje, y_koordinata_luknje = igra.pozicija_luknje
+    pozicija_luknje = igra.pozicija_luknje
+    igralec_na_vrsti = igra.igralec_na_vrsti
+    koordinate_zacetka = igra.koordinate_zacetka
+    runda = igra.runda
+    koncani_igralci = igra.koncani_igralci
+    return bottle.template("rezultati.html", igralci = igralci, runda=runda,koncani_igralci=koncani_igralci)
 
 bottle.run(reloader=True, debug=True)
